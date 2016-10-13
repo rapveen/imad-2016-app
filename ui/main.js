400205d6bac1c487948e0e4f88c7eee20568c4e6
rapveen <rapveen@users.noreply.github.com>
2016-10-13 09:30:21 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,9 +7,9 @@ element.innerHTML = 'newValue Praveen';
 var img = documment.getElementById('rap')
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft + 'px';
 }
 img.onclick = function(){
-  var interval = setInterval(moveRight,100);
+  var interval = setInterval(moveRight,50);
 };
