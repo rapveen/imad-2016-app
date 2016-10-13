fddc69b952e85f47572007b75e1c0d953b08454a
rapveen <rapveen@users.noreply.github.com>
2016-10-13 09:14:20 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,10 @@
 console.log('Loaded!');
-
+//change text to the main-tex div
 var element = document.getElementById('main-text');
 element.innerHTML = 'newValue Praveen';
+//move the image
+
+/*var img= document.getElementById('rap')
+img.onclick = function(){
+  img.stylr.marginLeft = '100px';  
+};*/
