8384b0b54e445261ceb0a507ed01d4cae69ec12f
rapveen <rapveen@users.noreply.github.com>
2016-10-28 07:09:40 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,6 +19,21 @@ button.onclick = function(){
   request.open('GET','http://rapveen.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+
+//Submit button
+var nameInput = document .getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  var names = ['name1', 'name2', 'name3', 'name4'];
+  var list = '';
+  for(var i=0;i<names.length;i++){
+      list+='<li>'+names[i]+'</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innnerHTML = list;
+};
+
 /*var counter = 0;
 button.onclick = function(){
   counter = counter + 1;  
