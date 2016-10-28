6dab6836913e66a2153e434505511d674f99105a
rapveen <rapveen@users.noreply.github.com>
2016-10-28 07:51:47 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,8 @@ button.onclick = function(){
      }
    };
    //Make the request
-  request.open('GET','http://rapveen.imad.hasura-app.io/counter',true);
-  request.send(null);
+   request.open('GET','http://rapveen.imad.hasura-app.io/counter',true);
+   request.send(null);
 };
 
 //Submit button
@@ -25,13 +25,25 @@ var nameInput = document .getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  for(var i=0;i<names.length;i++){
-      list+='<li>'+names[i]+'</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innnerHTML = list;
+  var request = new XMLHttpRequest();
+  
+  //Capture the response and store it in a variable
+  request.onreadystatechange = function(){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200){
+              var names =request.responseText;
+              names = JSON.parse(names);
+               var list = '';
+              for(var i=0;i<names.length;i++){
+                  list+='<li>'+names[i]+'</li>';
+               }
+             var ul = document.getElementById('namelist');
+            ul.innnerHTML = list;
+          }
+      }
+  };
+  request.open('GET','http://rapveen.imad.hasura-app.io/submit-name?name='+name,true);
+   request.send(null);
 };
 
 /*var counter = 0;
