fb18c379bc0935e3101039edce9cf2c8557983f9
rapveen <rapveen@users.noreply.github.com>
2016-10-28 06:47:04 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 //counter code
 var button = document.getElementById('counter');
-/*button.onclick = function(){
+button.onclick = function(){
     //Create the request object
     var request = new XMLHttpRequest();
     
@@ -18,10 +18,10 @@ var button = document.getElementById('counter');
    //Make the request
   request.open('GET','http://rapveen.imad.hasura-app.io/counter',true);
   request.send(null);
-};*/
-var counter = 0;
+};
+/*var counter = 0;
 button.onclick = function(){
   counter = counter + 1;  
   var span = document.getElementById('count');
   span.innerHTML = counter.toString();
-};
+};*/
